46d039da544a79c87509a5c1241c92286b96f2e6
Vysix <Tymenvis@outlook.com>
2022-10-06 20:13:06 +0200

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:ital,opsz,wght@0,8..144,100;0,8..144,200;0,8..144,300;0,8..144,400;0,8..144,500;1,8..144,100;1,8..144,200;1,8..144,300;1,8..144,400&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pompeji | Home</title>
+</head>
+<body>
+    <div class="landing-main">
+        <div class="landing-info-main">
+            <div class="landing-info-logo">
+                <img alt="Logo" src=" {{ URL::asset('images/logo-pompeji.png') }}">
+            </div>
+            <div class="landing-info-text">
+                <p>GROTE OPRUIMING!</p>
+                <div class="landing-info-sub-text-main">
+                    <div class="landing-info-sub-text">
+                        <p>Wegens omstandigheden houden wij een grote uitverkoop op bijna al onze producten! De kortingen kunnen oplopen tot wel meer dan 50%</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="landing-contact-main">
+            <div class="landing-contact-item">
+                <div class="item-icon">
+                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M35.6142 27.3014C34.2741 24.768 29.6205 22.0211 29.4156 21.9009C28.8176 21.5606 28.1936 21.3804 27.6096 21.3804C26.7414 21.3804 26.0306 21.7781 25.6002 22.5016C24.9194 23.3158 24.0752 24.2675 23.8703 24.415C22.2846 25.4908 21.0433 25.3687 19.6698 23.9952L12.0042 16.3289C10.6394 14.9641 10.5139 13.7075 11.5824 12.1304C11.7319 11.9242 12.6836 11.0793 13.4978 10.3979C14.017 10.0889 14.3734 9.62975 14.5296 9.06648C14.7372 8.31701 14.5843 7.4354 14.0945 6.57647C13.979 6.37893 11.2307 1.7246 8.69867 0.385157C8.22616 0.134889 7.69359 0.00274658 7.15968 0.00274658C6.28007 0.00274658 5.45251 0.345782 4.83051 0.967116L3.13669 2.66027C0.457814 5.33848 -0.511894 8.37441 0.252927 11.6833C0.890946 14.4409 2.75028 17.3754 5.7802 20.4047L15.5941 30.2185C19.4289 34.0533 23.0848 35.9981 26.4604 35.9981C26.4604 35.9981 26.4604 35.9981 26.4611 35.9981C28.9437 35.9981 31.2582 34.9429 33.3385 32.8627L35.0316 31.1696C36.0607 30.1411 36.2943 28.5861 35.6142 27.3014Z" fill="white"/>
+                    </svg>                        
+                </div>
+                <div class="item-text">
+                    +31 6 123456789
+                </div>
+            </div>
+            <div class="landing-contact-item">
+                <div class="item-icon">
+                    <svg width="32" height="42" viewBox="0 0 32 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16.0342 0H15.9657C7.67617 0 0.932373 5.89885 0.932373 13.1494C0.932373 17.9021 3.40605 24.1746 8.28467 31.7928C11.9017 37.4407 15.5694 41.7664 15.6061 41.8094C15.7082 41.9294 15.8691 42.0001 16.0396 42.0001C16.0444 42.0001 16.0494 42.0001 16.0542 41.9999C16.2302 41.9957 16.3925 41.9169 16.4901 41.7886C16.5263 41.7407 20.1678 36.9289 23.7603 31.0436C28.6091 23.1 31.0677 17.0794 31.0677 13.1494C31.0674 5.89875 24.3235 0 16.0342 0ZM22.9734 13.4707C22.9734 16.8354 19.8451 19.5725 15.9998 19.5725C12.1546 19.5725 9.02637 16.8353 9.02637 13.4707C9.02637 10.1062 12.1546 7.36898 15.9998 7.36898C19.8451 7.36898 22.9734 10.1062 22.9734 13.4707Z" fill="white"/>
+                    </svg>                        
+                </div>
+                <div class="item-text">
+                    Noorderbaan 13,<br>
+                    8256 PP Biddinghuizen 
+                </div>
+            </div>
+            <div class="landing-contact-item">
+                <div class="item-icon">
+                    <svg width="51" height="37" viewBox="0 0 51 37" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M51 35.6989V1.4524L33.5597 18.9556L51 35.6989Z" fill="white"/>
+                        <path d="M0 1.44985V35.6989L17.4411 18.9548L0 1.44985Z" fill="white"/>
+                        <path d="M1.17896 0.224998L19.0307 18.143C19.1335 18.1863 19.431 18.4958 19.4701 18.5842L24.52 23.6527C25.0597 24.1551 25.9412 24.1542 26.503 23.6314L31.5274 18.5884C31.5665 18.4991 31.8674 18.1863 31.9711 18.143L49.8228 0.224998H1.17896Z" fill="white"/>
+                        <path d="M32.3595 20.1609L27.6845 24.8529C27.064 25.4309 26.276 25.719 25.4915 25.719C24.7146 25.719 23.9419 25.4368 23.3384 24.875L18.6413 20.1609L1.33365 36.775H49.6663L32.3595 20.1609Z" fill="white"/>
+                    </svg>                        
+                </div>
+                <div class="item-text">
+                    Info@Pompeji.nl
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content-main">
+        <div class="content-information">
+            <h2>Interesse in een product?</h2>
+            <p>
+                Neem dan contact met ons op of kom bij ons langs!<br>
+                Openings tijden: vrijdag - zaterdag van 10:00 - 16:00
+            </p>
+        </div>
+        <div class="content-products">
+            @foreach($products as $product)
+            <div class="product-item">
+                <div class="product-image">
+                    <img alt="product_image" src="{{ $product->image_url }}">
+                </div>
+                <div class="product-info">
+                    <div class="product-title">
+                        {{ $product->name }}
+                    </div>
+                    <div class="product-price">
+                        <p class="old_price">was: &euro; {{ $product->old_price }},-</p>
+                        <p class="price">&euro; {{ $product->new_price }},-</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
