071ff7ce0f8dad1519caba8e28df68a10d423160
Vysix <Tymenvis@outlook.com>
2022-10-15 12:47:25 +0200

    small change added pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        $getProducts = Product::all();
+        $getProducts = Product::paginate(12);
         return view("index")->with("products", $getProducts);
     }
 
