71bbed1267c6df19a8a1e8de292b1796599431e9
Tymen Vis <Tymenvis@outlook.com>
2022-12-06 19:13:32 +0100

    Redirect

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ProductController::class, 'index']);
+Route::any('{url}', function(){
+    return redirect('/');
+})->where('url', '.*');
